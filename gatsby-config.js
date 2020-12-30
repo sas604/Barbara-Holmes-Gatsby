ec9d28c2eebbc6d9d3cb71f21b084525829fd52f
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2020-12-30 14:13:35 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: "Barbara Holmes ",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress-experimental",
+      options: {
+        url: " https://tagunovdesign.com/bh/graphql",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
