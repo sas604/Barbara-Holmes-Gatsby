3f6ded8ff135470b6459dbbd84f139d9ef52871c
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2021-01-03 15:31:19 -0800

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,29 @@
-module.exports = {
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
   siteMetadata: {
-    title: "Barbara Holmes ",
+    title: 'Barbara Holmes ',
   },
   plugins: [
     {
-      resolve: "gatsby-source-wordpress-experimental",
+      resolve: 'gatsby-source-wordpress-experimental',
       options: {
-        url: " https://tagunovdesign.com/bh/graphql",
+        url: ' https://tagunovdesign.com/bh/graphql',
       },
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
