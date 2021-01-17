99d815ff5efd7e3b473f7c8dd8db5d87e7a17e8d
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2021-01-17 15:10:50 -0800

    add favicon

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -17,8 +17,8 @@ export default function SEO({ children, location, description, title, image }) {
     <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
       <html lang="en" />
       <title>{title}</title>
-      {/* <link rel="icon" type="image/svg+xml" href="/favicon.sfg" />
-      <link rel="alternate icon" href="/favicon.ico" /> */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
       {/* Meta tags */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="utf-8" />
